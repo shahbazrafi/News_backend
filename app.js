353951ad6d4e7070eeb9abcd3ba6b33cf5854d76
shahbazrafi <rafi.10539@gmail.com>
2022-10-12 21:29:33 +0100

    Revert "5"

--- app.js
+++ app.js
@@ -4,23 +4,5 @@ const app = express()
 const controller = require("./controller")
 
 app.get("/api/topics", controller.getTopics)
-app.get("/api/articles/:article_id", controller.getArticleById)
-app.get("/api/users", controller.getUsers)
-
-app.use((err, req, res, next) => {
-    if (err.code === "22P02") {
-        res.status(400).send({message: "bad request"})
-    }
-    else next(err)
-})
-app.use((err, req, res, next) => {
-    if (err.message && err.status) {
-        res.status(err.status).send({message: err.message})
-    }
-    else next(err)
-})
-app.use((err, req, res, next) => {
-    res.status(500).send("server error")
-})
 
 module.exports  = app
--- model.js
+++ model.js
@@ -5,20 +5,3 @@ exports.selectTopics = () => {
     return db.query("SELECT * FROM topics")
     .then(data => data.rows)
 }
-
-exports.selectArticleById = (article_id) => {
-    
-    return db.query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
-    .then(({rows}) => {
-        if (rows.length ===0){
-            return Promise.reject({status: 404, message: "no article_id found"})
-        } else {
-            return rows[0]
-        }
-    })
-}
-
-exports.selectUsers = () => {
-    return db.query("SELECT * FROM users")
-    .then(data => data.rows)
-}
