5e9a1d188e54f5d802768293f9642d198760f8b2
shahbazrafi <rafi.10539@gmail.com>
2022-10-14 02:32:30 +0100

    task 7 feeback (to be refactored later)

--- model.js
+++ model.js
@@ -13,7 +13,6 @@ exports.selectArticleById = (article_id) => {
         if (rows.length ===0){
             return Promise.reject({status: 404, message: "no article_id found"})
         } else {
-            console.log(rows[0], article_id)
             return db.query(`SELECT * FROM comments WHERE article_id=$1`, [article_id])
             .then(data => {
                 rows[0].comment_count=data.rows.length
